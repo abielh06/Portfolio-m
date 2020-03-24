931ad16d5dfd02e76451f987c5734d2e15239c2e
Abiel Hailemariam <abielh06@gmail.com>
2020-03-23 22:41:45 -0500

    minor styling changes

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,21 @@
-$(document).ready(function(){
-    $('.parallax').parallax();
-});
+// $(document).ready(function(){
+//     $('.parallax').parallax();
+// });
 
-$(document).ready(function(){
-    $('.fixed-action-btn').floatingActionButton();
-  })
+// $(document).ready(function(){
+//     $('.fixed-action-btn').floatingActionButton();
+//   })
+
+
+//   function start() {
+//     $('.parallax').parallax();
+//     $('.fixed-action-btn').floatingActionButton();
+//   };
+
+
+const start = () => {
+    $('.parallax').parallax();
+    $('.fixed-action-btn').floatingActionButton(); 
+         
+}
+start();
